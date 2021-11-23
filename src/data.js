7c7b589f747f22c3cd9edcc58b88601c67fd9297
alejandropascuale <apascuale@outlook.com>
2021-11-23 08:58:02 -0300

    install momment to format dates

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ export default [
     id: 1,
     name: 'Bertie Yates',
     age: 29,
+    date: '24/06/2021',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
@@ -10,6 +11,7 @@ export default [
     id: 2,
     name: 'Hester Hogan',
     age: 32,
+    date: '06/10/1966',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
@@ -17,6 +19,7 @@ export default [
     id: 3,
     name: 'Larry Little',
     age: 36,
+    date: '18/08/1970',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
@@ -24,6 +27,7 @@ export default [
     id: 4,
     name: 'Sean Walsh',
     age: 34,
+    date: '18/10/1991',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
@@ -31,6 +35,7 @@ export default [
     id: 5,
     name: 'Lola Gardner',
     age: 29,
+    date: '18/03/1996',
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
