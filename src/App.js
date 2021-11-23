0cb4d0fbb4dd7017025d63d3f869bb3edd043dfe
alejandropascuale <apascuale@outlook.com>
2021-11-23 08:45:01 -0300

    create List component and add prop to get data from json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React, { useState } from 'react';
 import './App.css';
+import data from './data'
+import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      
-    </div>
+    <main>
+      <section className='container'>
+        <h3>{people.length} bithdays today</h3>
+        <List people={people}/>
+        <button onClick={ () => console.log('you click me!!!')}>clear all</button>
+      </section>
+    </main>
   );
 }
 
